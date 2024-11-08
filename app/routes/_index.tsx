c4e8055892563074e776b2ebc2552fa7dfbde01e
tanakalucky <39301079+tanakalucky@users.noreply.github.com>
2024-11-08 22:09:42 +0900

    chore: Change meta data

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,8 +2,8 @@ import type { MetaFunction } from '@remix-run/cloudflare';
 
 export const meta: MetaFunction = () => {
   return [
-    { title: 'New Remix App' },
-    { name: 'description', content: 'Welcome to Remix!' },
+    { title: 'Murder Mistery Note' },
+    { name: 'description', content: 'This is note app for murder mistery' },
   ];
 };
 
