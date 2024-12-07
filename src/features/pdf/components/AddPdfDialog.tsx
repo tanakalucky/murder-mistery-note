3dc37e4a874fcbd35f879ed42f911ebad66795f5
tanakalucky <39301079+tanakalucky@users.noreply.github.com>
2024-12-07 11:31:04 +0900

    chore: Can close dialog with only the close button

--- src/features/pdf/components/AddPdfDialog.tsx
+++ src/features/pdf/components/AddPdfDialog.tsx
@@ -1,5 +1,5 @@
 import { Button } from '@/components/ui/button';
-import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle } from '@/components/ui/dialog';
+import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { SidebarGroupAction } from '@/components/ui/sidebar';
@@ -13,13 +13,8 @@ export default function AddPdfDialog() {
   const [url, setUrl] = useState('');
   const { pdfList, addPdf } = usePdfStore();
 
-  const handleClick = () => {
-    setOpen(true);
-  };
-
   const handleChangeName = (e: ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
-    if (!value) return;
     setName(value);
   };
 
@@ -31,45 +26,56 @@ export default function AddPdfDialog() {
 
   const handleAddPdf = () => {
     addPdf([...pdfList, { name, url }]);
+    handleClose();
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    clearValues();
+  };
+
+  const clearValues = () => {
     setName('');
     setUrl('');
-    setOpen(false);
   };
 
   return (
-    <>
-      <SidebarGroupAction title='Add PDF' onClick={handleClick}>
-        <Plus /> <span className='sr-only'>Add PDF</span>
-      </SidebarGroupAction>
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <SidebarGroupAction title='Add PDF'>
+          <Plus /> <span className='sr-only'>Add PDF</span>
+        </SidebarGroupAction>
+      </DialogTrigger>
 
-      <Dialog open={open} onOpenChange={setOpen}>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>PDFを追加</DialogTitle>
-          </DialogHeader>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>PDFを追加</DialogTitle>
+        </DialogHeader>
 
-          <div className='grid gap-4 py-4'>
-            <div className='grid grid-cols-4 items-center gap-4'>
-              <Label htmlFor='name' className='text-right'>
-                ファイル名
-              </Label>
-              <Input id='name' className='col-span-3' value={name} onChange={handleChangeName} />
-            </div>
-            <div className='grid grid-cols-4 items-center gap-4'>
-              <Label htmlFor='pdf' className='text-right'>
-                PDFファイル
-              </Label>
-              <Input id='pdf' className='col-span-3' type='file' accept='application/pdf' onChange={handleChangeFile} />
-            </div>
+        <div className='grid gap-4 py-4'>
+          <div className='grid grid-cols-4 items-center gap-4'>
+            <Label htmlFor='name' className='text-right'>
+              ファイル名
+            </Label>
+            <Input id='name' className='col-span-3' value={name} onChange={handleChangeName} />
           </div>
+          <div className='grid grid-cols-4 items-center gap-4'>
+            <Label htmlFor='pdf' className='text-right'>
+              PDFファイル
+            </Label>
+            <Input id='pdf' className='col-span-3' type='file' accept='application/pdf' onChange={handleChangeFile} />
+          </div>
+        </div>
 
-          <DialogFooter>
-            <Button type='button' onClick={handleAddPdf}>
-              追加
-            </Button>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
-    </>
+        <DialogFooter className='flex gap-4'>
+          <Button type='button' variant='secondary' onClick={handleClose}>
+            閉じる
+          </Button>
+          <Button type='submit' onClick={handleAddPdf}>
+            追加
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 }
